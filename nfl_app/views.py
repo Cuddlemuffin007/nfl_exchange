7f035e0752afbca0c910b47892cefb4b057a4fd5
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-03-23 16:44:00 -0400

    added answer list to question_detail_view

--- nfl_app/views.py
+++ nfl_app/views.py
@@ -22,6 +22,5 @@ class QuestionDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['quesion_answers'] = Answer.objects.filter(question=self.kwargs.get('pk'))
-        print(context)
+        context['question_answers'] = Answer.objects.filter(question=self.kwargs.get('pk'))
         return context
