5569949d1a04c3b305ad9723bad7c57f0edf9782
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-03-24 18:54:31 -0400

    little typo in settings.py

--- nfl_exchange/settings.py
+++ nfl_exchange/settings.py
@@ -130,6 +130,6 @@ REST_FRAMEWORK = {
             'rest_framework.authentication.TokenAuthentication',
             ),
         'DEFAULT_PERMISSION_CLASSES': (
-            'rest_framework_permissions.IsAuthenticated',
+            'rest_framework.permissions.IsAuthenticated',
             )
         }
