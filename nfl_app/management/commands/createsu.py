81f8f2d249b0f9fc01267bd4d7366827a433eafb
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-05-08 15:54:23 -0400

    extracted superuser credentials to environment varialbles for eb deployment

--- nfl_app/management/commands/createsu.py
+++ nfl_app/management/commands/createsu.py
@@ -1,3 +1,4 @@
+import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
@@ -5,5 +6,6 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@admin.com', 'safepass')
+        username = os.environ.get('SU_USERNAME')
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, password=os.environ.get('SU_PASSWORD'))
